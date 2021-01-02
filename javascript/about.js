2c6cdb757b312a4aa7e6cfd6e2c200c6cbf63b17
DUT_gaoxinzhi <651599228@qq.com>
2021-01-02 19:24:50 +0800

    update

---
+++ javascript/about.js
@@ -0,0 +1,10 @@
+$("#navbarsExampleDefault > ul > li:nth-child(1) > a").mPageScroll2id();
+$("#navbarsExampleDefault > ul > li:nth-child(2) > a").mPageScroll2id();
+$("#navbarsExampleDefault > ul > li:nth-child(3) > a").mPageScroll2id();
+$("#navbarsExampleDefault > ul > li:nth-child(4) > a").mPageScroll2id();
+$("#navbarsExampleDefault > ul > li:nth-child(5) > a").mPageScroll2id();
+$("#jumb > div > p:nth-child(3) > a").mPageScroll2id();
+$("body > nav > a").mPageScroll2id();
+
+
+
